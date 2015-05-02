6b16dc628b348f4c1e41b58f3173abffd307c983
Panagiotis Roubatsis <proubatsis@gmail.com>
2015-05-01 20:03:22 -0400

    Added license.

--- Procedural City/CustomNode.cpp
+++ Procedural City/CustomNode.cpp
@@ -1,3 +1,21 @@
+/*
+    Copyright (C) 2015 Panagiotis Roubatsis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include "CustomNode.h"
 
 CustomNode::CustomNode(std::vector<irr::video::S3DVertex> vertices, std::vector<irr::u16> indices, irr::core::aabbox3df box, irr::scene::ISceneManager* sceneManager, ISceneNode* parent, int id)
